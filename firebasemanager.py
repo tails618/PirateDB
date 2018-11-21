36574ef32197d3618bd5e18e1f361d583411721c
Asher Grossman <asher.grossman@gmail.com>
2018-11-20 19:23:24 -0600

    ADDED A WHOLE LOT OF STUFF!

---
+++ firebasemanager.py
@@ -0,0 +1,8 @@
+from firebase import firebase as fb
+class FirebaseManager:
+    app=fb.FirebaseApplication("https://piratedb-f3ec8.firebaseio.com/")
+    def writeToFile(self,idNum,obj):
+        result=self.app.put("",idNum,obj)
+    def getAllPirates(self):
+        d=self.app.get("",None)
+        return d
---
+++ userinterface.py
@@ -0,0 +1,63 @@
+from tkinter import *
+import firebasemanager
+
+window1=Tk()
+
+#Make the stuff
+frame1=Frame(window1)
+frame2=Frame(window1)
+frame3=Frame(window1)
+frame4=Frame(window1)
+
+label1=Label(frame1,text="Pirate Database",font=("Comic Sans MS", 12))
+label1.pack()
+
+label2=Label(frame2,text="Search",font=("Comic Sans MS", 12))
+label2.grid(row=0,column=0)
+
+entry=Entry(frame2)
+entry.grid(row=0,column=1)
+
+searchButton=Button(frame2,text="Go",font=("Comic Sans MS", 12))
+searchButton.grid(row=0,column=2)
+
+label3=Label(frame3,text="Frame 3",font=("Comic Sans MS", 12))
+label3.grid(row=0,column=0)
+leftImg=PhotoImage(file="arrow_left.gif")
+rightImg=PhotoImage(file="arrow_right.gif")
+leftButton=Button(frame3,image=leftImg)
+rightButton=Button(frame3,image=rightImg)
+leftButton.grid(row=1,column=0)
+rightButton.grid(row=1,column=2)
+placeholder=PhotoImage(file="me.gif")
+piratePic=Label(frame3,image=placeholder,text="ARRRRRR #teampixel")
+piratePic.grid(row=1,column=1)
+shipLabel=Label(frame3,text="Ship Name",font=("Comic Sans MS",12))
+shipLabel.grid(row=2,column=1)
+ficLabel=Label(frame3,text="Fictional?",font=("Comic Sans MS",12))
+ficLabel.grid(row=3,column=1)
+
+listbox=Listbox(frame4,font=("Comic Sans MS",12))
+listbox.pack()
+fm=firebasemanager.FirebaseManager()
+d=fm.getAllPirates()
+for item in d:
+    pirate=d[item]
+    listbox.insert(EMD,pirate["name"])
+
+def listDelete():
+    listbox.delete(ANCHOR)
+deleteButton=Button(frame4,text="Delete",font=("Comic Sans MS", 12),command=listDelete)
+deleteButton.pack()
+def quitWindow():
+    window1.destroy()
+exitButton=Button(frame4,text="Exit",font=("Comic Sans MS",12),command=quitWindow)
+exitButton.pack()
+
+#Grid the stuff
+frame1.grid(row=0,column=0)
+frame2.grid(row=0,column=1)
+frame3.grid(row=1,column=0)
+frame4.grid(row=1,column=1)
+
+window1.mainloop()
