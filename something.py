5e77c050930ede579c05d0b817a36a80ab29657b
Asher Grossman <asher.grossman@gmail.com>
2019-01-08 19:16:03 -0600

    yup

---
+++ something.py
@@ -0,0 +1,14 @@
+from google.cloud import storage
+import os
+class ImageManager:
+#this tells the project where to find the credentials we downloaded
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="pirateDB-a1c1dd245fa5.json"
+    client=storage.Client()
+    bucket=client.get_bucket('piratedb-f3ec8.appspot.com')
+    url=''
+    imagepath="me.gif"
+    def uploadImage(self):
+        imageBlob=bucket.blob("images/"+imagepath)
+        imageBlob.upload_from_filename(imagepath)
+        d=datetime.datetime(2006,12,18)
+        self.url=imageBlob.generate_signed_url(d)
